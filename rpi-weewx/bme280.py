adb788d0b238cd43b80327b497dc284372707fcb
Brendan O'Donnell <brendan.james.odonnell@gmail.com>
2017-10-28 04:40:18 +0000

    The BME280 service shouldnt stop weewx from starting

--- rpi-weewx/bme280.py
+++ rpi-weewx/bme280.py
@@ -9,21 +9,32 @@ class BME280Service(StdService):
     def __init__(self, engine, config_dict):
         super(BME280Service, self).__init__(engine, config_dict)      
         d = config_dict.get('BME280Service', {})
-        self.sensor = BME280(t_mode=BME280_OSAMPLE_8,
-                             p_mode=BME280_OSAMPLE_8,
-                             h_mode=BME280_OSAMPLE_8)
+        self._init_sensor()
         self.converter = Converter()
         self.bind(weewx.NEW_ARCHIVE_RECORD, self.new_archive_record)
+
+    def _init_sensor(self):
+        try:
+            self.sensor = BME280(t_mode=BME280_OSAMPLE_8,
+                                 p_mode=BME280_OSAMPLE_8,
+                                 h_mode=BME280_OSAMPLE_8)
+        except Exception as e:
+            print 'BME280: error initializing sensor: {}'.format(e)
+            self.sensor = None
     
     def new_archive_record(self, event):
+        if self.sensor is None:
+            self._init_sensor()
         try:
-            sample = {'usUnits': weewx.METRIC,
-                      'inTemp': self.sensor.read_temperature(),
-                      'inHumidity': self.sensor.read_humidity(),
-                      'pressure': self.sensor.read_pressure()/100}
-            print 'BME280: raw sample: {}'.format(sample)
-            sample = self.converter.convertDict(sample)
-            print 'BME280: converted sample: {}'.format(sample)
+            assert self.sensor is not None, "sensor is not initialized"
+            sample = self.converter.convertDict({
+                'usUnits': weewx.METRIC,
+                'inTemp': self.sensor.read_temperature(),
+                'inHumidity': self.sensor.read_humidity(),
+                'pressure': self.sensor.read_pressure()/100,
+            })
+            print 'BME280: sample: {}'.format(sample)
             event.record.update(sample)
         except Exception as e:
-            print 'BME280: error: {}'.format(e)
+            print 'BME280: sampling error: {}'.format(e)
+            self.sensor = None
